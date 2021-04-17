b0d511a7c980dde9f965076dac555e7244d41842
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2021-04-17 17:04:39 -0400

    Update READ ME FIRST THEN RUN.py

--- LIB INSTALLER/READ ME FIRST THEN RUN.py
+++ LIB INSTALLER/READ ME FIRST THEN RUN.py
@@ -24,6 +24,6 @@ os.system("curl https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Wind
 os.system("https://downloads.arduino.cc/arduino-1.8.13-windows.exe") #install standard arduino IDE
 os.system("curl https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&rel=16") #installs visual studio
 arduino = ZipFile('arduino-cli_latest_Windows_32bit.zip') #creates extraction variable for arduino CLI
-arduino.extractall() #extracts arduino cli
+arduino.extractall() #extracts arduino CLI
 os.system("arduino-cli.exe") #runs arduino CLI
 os.system("arduino-1.8.13-windows.exe") #runs arduino IDE
